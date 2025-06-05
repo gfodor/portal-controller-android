dee3acb95d5bc21bcb705ad1a93dc5cdeb1f301f
Greg Fodor <gfodor@gmail.com>
2025-06-04 23:06:57 -0500

    Remove ArFragment usage and use direct ARCore session

--- app/src/main/java/com/example/alex/arcore_rosbridge/MainActivity.java
+++ app/src/main/java/com/example/alex/arcore_rosbridge/MainActivity.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
-import android.view.MotionEvent;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -22,37 +21,25 @@ import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import com.google.ar.core.Anchor;
-import com.google.ar.core.AugmentedImage;
+import com.google.ar.core.Config;
 import com.google.ar.core.Frame;
+import com.google.ar.core.Pose;
+import com.google.ar.core.Session;
 import com.google.ar.core.TrackingState;
-import com.google.ar.sceneform.AnchorNode;
-import com.google.ar.sceneform.Node;
-import com.google.ar.sceneform.math.Quaternion;
-import com.google.ar.sceneform.math.Vector3;
-import com.google.ar.sceneform.ux.ArFragment;
 import com.jilk.ros.ROSClient;
 import com.jilk.ros.Topic;
-import com.jilk.ros.message.Pose;
 import com.jilk.ros.rosbridge.ROSBridgeClient;
 
-import org.json.JSONObject;
 
-import java.io.InputStream;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final double MIN_OPENGL_VERSION = 3.0;
 
-    private ArFragment arFragment;
-
+    private Session arSession;
     private boolean rosbridge_connected = false;
     private ROSBridgeClient rosclient;
 
@@ -61,10 +48,27 @@ public class MainActivity extends AppCompatActivity {
     private Button connect_btn;
     private Button calibration_btn;
 
-    private Map<Integer, float[]> image_pos_dict;
-    private Node map_node;
-    private Node cam_node;
-    private Boolean map_calibrated = false;
+    private float[] cam_pos = new float[3];
+    private float[] cam_quat = new float[4];
+
+    private android.opengl.GLSurfaceView glSurfaceView;
+    private android.os.Handler frameHandler = new android.os.Handler();
+    private final Runnable frameUpdate = new Runnable() {
+        @Override public void run() {
+            if (arSession != null) {
+                try {
+                    Frame frame = arSession.update();
+                    if (frame.getCamera().getTrackingState() == TrackingState.TRACKING) {
+                        Pose pose = frame.getCamera().getDisplayOrientedPose();
+                        pose.getTranslation(cam_pos, 0);
+                        pose.getRotationQuaternion(cam_quat, 0);
+                        updatePoseViews();
+                    }
+                } catch (Exception ignored) {}
+            }
+            frameHandler.postDelayed(this, 33);
+        }
+    };
 
     private TextView pos_x_txt;
     private TextView pos_y_txt;
@@ -89,41 +93,14 @@ public class MainActivity extends AppCompatActivity {
         }
         setContentView(R.layout.activity_main);
 
-        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
-
+        glSurfaceView = findViewById(R.id.arcore_gl_surface);
         rosmaster_ip_txt = findViewById(R.id.rosmaster_ip_txt);
         rosbridge_port_txt = findViewById(R.id.rosbridge_port_txt);
         connect_btn = findViewById(R.id.connect_btn);
         calibration_btn = findViewById(R.id.calibration_btn);
-        // click still resets map as before
-        calibration_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Reset calibration so the next marker re-establishes map alignment
-                map_calibrated = false;
-                map_node.setParent(null);
-                cam_node.setParent(null);
-                Toast.makeText(getApplicationContext(),
-                        "Calibration reset – point camera at marker",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
 
-        calibration_btn.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        // Calibration TODO
-                        return true;
-                }
-                return false;
-            }
-        });
-
-        image_pos_dict = new HashMap<>();
-        map_node = new Node();
-        cam_node = new Node();
+        calibration_btn.setOnClickListener(v ->
+                Toast.makeText(getApplicationContext(), "Calibration reset", Toast.LENGTH_SHORT).show());
 
         pos_x_txt = findViewById(R.id.pos_x_txt);
         pos_y_txt = findViewById(R.id.pos_y_txt);
@@ -136,33 +113,15 @@ public class MainActivity extends AppCompatActivity {
         rosmaster_ip_txt.clearFocus();
         rosbridge_port_txt.clearFocus();
 
-        // Load image map json file
         try {
-            InputStream is = getAssets().open("augmentedImages.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            JSONObject map_json = new JSONObject(new String(buffer, "UTF-8"));
-            for(int i=0; i < map_json.getJSONArray("AugmentedImages").length(); i++) {
-                String img_str = map_json.getJSONArray("AugmentedImages").get(i).toString();
-                JSONObject img_json = new JSONObject(img_str);
-                JSONObject img_pos = img_json.getJSONObject("Position");
-                JSONObject img_rot = img_json.getJSONObject("Rotation");
-                Quaternion img_quat = new Quaternion(new Vector3((float) img_rot.getDouble("rx") , (float) img_rot.getDouble("ry"), (float) img_rot.getDouble("rz")));
-                float[] img_pose = new float[7];
-                img_pose[0] = (float) img_pos.getDouble("x");
-                img_pose[1] = (float) img_pos.getDouble("y");
-                img_pose[2] = (float) img_pos.getDouble("z");
-                img_pose[3] = img_quat.x;
-                img_pose[4] = img_quat.y;
-                img_pose[5] = img_quat.z;
-                img_pose[6] = img_quat.w;
-                image_pos_dict.put(img_json.getInt("Id"), img_pose);
-            }
-        }
-        catch (Exception ex) {
-            Toast toast = Toast.makeText(this, "Unable to load images map", Toast.LENGTH_LONG);
+            arSession = new Session(this);
+            Config config = new Config(arSession);
+            config.setPlaneFindingMode(Config.PlaneFindingMode.DISABLED);
+            config.setCloudAnchorMode(Config.CloudAnchorMode.DISABLED);
+            config.setLightEstimationMode(Config.LightEstimationMode.DISABLED);
+            arSession.configure(config);
+        } catch (Exception ex) {
+            Toast toast = Toast.makeText(this, "ARCore unavailable: " + ex.getMessage(), Toast.LENGTH_LONG);
             toast.setGravity(Gravity.CENTER, 0, 0);
             toast.show();
         }
@@ -181,15 +140,9 @@ public class MainActivity extends AppCompatActivity {
                 rosbridge_connected = rosclient.connect(new ROSClient.ConnectionStatusListener() {
                     @Override
                     public void onConnect() {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                if(rosbridge_connected) {
-                                    Toast.makeText(getApplicationContext(), "Connected to ROS", Toast.LENGTH_LONG).show();
-                                }
-                                FragmentTransaction tr = arFragment.getFragmentManager().beginTransaction();
-                                tr.replace(R.id.ux_fragment, arFragment);
-                                tr.commit();
+                        runOnUiThread(() -> {
+                            if (rosbridge_connected) {
+                                Toast.makeText(getApplicationContext(), "Connected to ROS", Toast.LENGTH_LONG).show();
                             }
                         });
                     }
@@ -214,84 +167,6 @@ public class MainActivity extends AppCompatActivity {
                         });
                     }
                 });
-            }
-        });
-
-        arFragment.getArSceneView().getScene().addOnUpdateListener(frameTime -> {
-            Frame frame = arFragment.getArSceneView().getArFrame();
-            // If there is no frame, just return.
-            if (frame == null) {
-                return;
-            }
-            if (frame.getCamera().getTrackingState() != TrackingState.TRACKING) {
-                return;
-            }
-
-            Collection<AugmentedImage> updatedAugmentedImages = frame.getUpdatedTrackables(AugmentedImage.class);
-            for (AugmentedImage augmentedImage : updatedAugmentedImages) {
-                switch (augmentedImage.getTrackingMethod()) {
-                    case NOT_TRACKING:
-                        break;
-                    case LAST_KNOWN_POSE:
-                        break;
-                    case FULL_TRACKING: // Update only when image in the camera view
-                        Anchor imageAnchor = augmentedImage.createAnchor(augmentedImage.getCenterPose());
-                        AnchorNode imageAnchorNode = new AnchorNode(imageAnchor);
-                        imageAnchorNode.setParent(arFragment.getArSceneView().getScene());
-                        map_node.setParent(imageAnchorNode);
-                        // Add local transformation
-                        float[] img_map_pose = image_pos_dict.get(augmentedImage.getIndex());
-                        map_node.setLocalPosition(new Vector3(img_map_pose[0], img_map_pose[1], img_map_pose[2]));
-                        map_node.setLocalRotation(new Quaternion(-img_map_pose[3], -img_map_pose[4], -img_map_pose[5], img_map_pose[6]));
-
-                        Vector3 map_trans = map_node.getWorldPosition();
-                        Quaternion map_quat = map_node.getWorldRotation();
-                        float[] map_trans_float = {map_trans.x, map_trans.y, map_trans.z};
-                        float[] map_quat_float = {map_quat.x, map_quat.y, map_quat.z, map_quat.w};
-                        com.google.ar.core.Pose map_pose = new com.google.ar.core.Pose(map_trans_float, map_quat_float);
-                        Anchor mapAnchor = augmentedImage.createAnchor(map_pose);
-                        AnchorNode mapAnchorNode = new AnchorNode(mapAnchor);
-                        mapAnchorNode.setParent(arFragment.getArSceneView().getScene());
-                        cam_node.setParent(mapAnchorNode);
-                        map_calibrated = true;
-                        break;
-                }
-
-            }
-
-            float[] trans =  arFragment.getArSceneView().getArFrame().getCamera().getDisplayOrientedPose().getTranslation();
-            float[] quat =  arFragment.getArSceneView().getArFrame().getCamera().getDisplayOrientedPose().getRotationQuaternion();
-
-            cam_node.setWorldPosition(new Vector3(trans[0], trans[1], trans[2]));
-            cam_node.setWorldRotation(new Quaternion(quat[0], quat[1], quat[2], quat[3]));
-
-            pos_x_txt.setText(String.format ("%.2f", cam_node.getLocalPosition().x));
-            pos_y_txt.setText(String.format ("%.2f", cam_node.getLocalPosition().y));
-            pos_z_txt.setText(String.format ("%.2f", cam_node.getLocalPosition().z));
-            rot_x_txt.setText(String.format ("%.2f", cam_node.getLocalRotation().x));
-            rot_y_txt.setText(String.format ("%.2f", cam_node.getLocalRotation().y));
-            rot_z_txt.setText(String.format ("%.2f", cam_node.getLocalRotation().z));
-            rot_w_txt.setText(String.format ("%.2f", cam_node.getLocalRotation().w));
-
-            if(rosbridge_connected && map_calibrated) {
-                Topic<Pose> cam_map_topic = new com.jilk.ros.Topic<Pose>("/arcore/cam_pose", Pose.class, rosclient);
-                cam_map_topic.advertise();
-                Pose odom_map_msg = new Pose();
-                odom_map_msg.position.x = cam_node.getLocalPosition().x;
-                odom_map_msg.position.y = cam_node.getLocalPosition().y;
-                odom_map_msg.position.z = cam_node.getLocalPosition().z;
-                odom_map_msg.orientation.x = cam_node.getLocalRotation().x;
-                odom_map_msg.orientation.y = cam_node.getLocalRotation().y;
-                odom_map_msg.orientation.z = cam_node.getLocalRotation().z;
-                odom_map_msg.orientation.w = cam_node.getLocalRotation().w;
-                cam_map_topic.publish(odom_map_msg);
-            }
-
-            if (bleClient != null) {
-                Vector3 p = cam_node.getLocalPosition();
-                Quaternion q = cam_node.getLocalRotation();
-                bleClient.sendPose(new float[]{p.x, p.y, p.z},
-                                   new float[]{q.x, q.y, q.z, q.w});
             }
         });
 
@@ -368,6 +243,52 @@ public class MainActivity extends AppCompatActivity {
                 new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
                 REQ_BLE_PERMS
             );
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (arSession != null) {
+            try { arSession.resume(); } catch (Exception ignored) {}
+        }
+        frameHandler.post(frameUpdate);
+    }
+
+    @Override
+    protected void onPause() {
+        frameHandler.removeCallbacks(frameUpdate);
+        if (arSession != null) {
+            arSession.pause();
+        }
+        super.onPause();
+    }
+
+    private void updatePoseViews() {
+        pos_x_txt.setText(String.format("%.2f", cam_pos[0]));
+        pos_y_txt.setText(String.format("%.2f", cam_pos[1]));
+        pos_z_txt.setText(String.format("%.2f", cam_pos[2]));
+        rot_x_txt.setText(String.format("%.2f", cam_quat[0]));
+        rot_y_txt.setText(String.format("%.2f", cam_quat[1]));
+        rot_z_txt.setText(String.format("%.2f", cam_quat[2]));
+        rot_w_txt.setText(String.format("%.2f", cam_quat[3]));
+
+        if (rosbridge_connected) {
+            Topic<com.jilk.ros.message.Pose> cam_topic = new Topic<>("/arcore/cam_pose", com.jilk.ros.message.Pose.class, rosclient);
+            cam_topic.advertise();
+            com.jilk.ros.message.Pose msg = new com.jilk.ros.message.Pose();
+            msg.position.x = cam_pos[0];
+            msg.position.y = cam_pos[1];
+            msg.position.z = cam_pos[2];
+            msg.orientation.x = cam_quat[0];
+            msg.orientation.y = cam_quat[1];
+            msg.orientation.z = cam_quat[2];
+            msg.orientation.w = cam_quat[3];
+            cam_topic.publish(msg);
+        }
+
+        if (bleClient != null) {
+            bleClient.sendPose(cam_pos, cam_quat);
         }
     }
 
